c5019f90248183cb990eb34a6ea2b10149e14e61
DunEideann <samuel.rys@gmail.com>
2021-04-02 23:24:14 -0300

    tenth commit

--- personal/__init__.py
+++ personal/__init__.py
@@ -1,5 +1,4 @@
 import os
-
 from flask import Flask, render_template, request
 from flask_mail import Message, Mail
 
--- personal/models.py
+++ personal/models.py
@@ -1,3 +1,4 @@
+import os
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
